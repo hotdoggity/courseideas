543d28eb1e5829b1f644c468042e39b581f30fba
Matt Alexander <mizatt@gmail.com>
2018-04-18 23:11:37 -0700

    Some more shiznit

--- src/main/java/com/teamtreehouse/courses/Main.java
+++ src/main/java/com/teamtreehouse/courses/Main.java
@@ -1,5 +1,8 @@
 package com.teamtreehouse.courses;
 
+import com.teamtreehouse.courses.model.CourseIdea;
+import com.teamtreehouse.courses.model.CourseIdeaDAO;
+import com.teamtreehouse.courses.model.SimpleCourseIdeaDAO;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -8,16 +11,41 @@ import java.util.Map;
 
 import static spark.Spark.get;
 import static spark.Spark.post;
+import static spark.Spark.staticFileLocation;
 
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello world!");
-        get("/", (req, res) -> { return new ModelAndView(null, "index.hbs"); }, new HandlebarsTemplateEngine());
+        staticFileLocation("/public");
+        CourseIdeaDAO dao = new SimpleCourseIdeaDAO();
+
+        get("/", (req, res) -> {
+            Map<String, String> model = new HashMap<>();
+            model.put("username", req.cookie("username"));
+            return new ModelAndView(model, "index.hbs");
+            }, new HandlebarsTemplateEngine());
 
         post("/sign-in", (req, res) -> {
             Map<String, String> model = new HashMap<>();
-            model.put("username", req.queryParams("username"));
-            return new ModelAndView(model, "sign-in.hbs");
+            String username = req.queryParams("username");
+            res.cookie("username", username);
+//            model.put("username", username);
+            res.redirect("/");
+            return null;
+        }, new HandlebarsTemplateEngine());
+
+        get("/ideas", (req, res) -> {
+            Map<String, Object> model = new HashMap<>();
+            model.put("ideas", dao.findAll());
+            return new ModelAndView(model, "ideas.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        post("/ideas", (req, res) -> {
+            String title = req.queryParams("title");
+            //TODO: This username is tied to the cookie implementation
+            CourseIdea idea = new CourseIdea(title, req.cookie("username"));
+            dao.add(idea);
+            res.redirect("/ideas");
+            return null;
         }, new HandlebarsTemplateEngine());
     }
 }
---
+++ src/main/java/com/teamtreehouse/courses/model/CourseIdeaDAO.java
@@ -0,0 +1,8 @@
+package com.teamtreehouse.courses.model;
+
+import java.util.List;
+
+public interface CourseIdeaDAO {
+    boolean add(CourseIdea idea);
+    List<CourseIdea> findAll();
+}
